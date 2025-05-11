8993436dd8992c9b68580e082552a88df497e766
Aalok Kumar Rajwade <aalokkr.rajwade.cer21@itbhu.ac.in>
2025-05-11 18:39:10 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+let score1 = "33";
+// let score1 = null;
+// let score1 = true;
+// let score1 = undefined;
+
+console.log(score1);
+console.log(typeof score1);
+// console.log(typeof(score)); both the ways are same for determining the type of datatype
+
+let newScore1 = Number(score1);
+console.log(newScore1);
+console.log(typeof newScore1);
+
+let score2 = "33abc";
+console.log(score2);
+console.log(typeof score2);
+// console.log(typeof(score)); both the ways are same for determining the type of datatype
+
+let newScore2 = Number(score2);
+console.log(newScore2); // Nan -> not a number
+console.log(typeof newScore2); 
+
+// conversion in number
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let score3 = 2;
+console.log(score3);
+console.log(typeof score3);
+// console.log(typeof(score)); both the ways are same for determining the type of datatype
+
+let newScore3 = Boolean(score3);
+console.log(newScore3); // Nan -> not a number
+console.log(typeof newScore3);
+
+// conversion in boolean:
+// 1 => true
+// 2 => true
+// 0 => false
+// "" => false
+// "abc" => true
