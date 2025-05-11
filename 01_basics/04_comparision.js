77a1be57a075861914628af340581b61a2b1ac57
Aalok Kumar Rajwade <aalokkr.rajwade.cer21@itbhu.ac.in>
2025-05-11 18:58:46 +0000

    comparators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2!=1)
+// console.log(2==1)
+// console.log(2<1)
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+// equality check == and comparision check like >= or > or <= or < works differently, 
+// comparision operation converts null to a number
+
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >= 0)
+
+console.log("2" == 2)
+
+// === is called strict check:
+console.log("2" === 2) // this checks data type also
