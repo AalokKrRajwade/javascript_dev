9239c53321737719652291bbf54834f50ccd408b
Aalok Kumar Rajwade <aalokkr.rajwade.cer21@itbhu.ac.in>
2025-05-18 10:25:25 +0000

    math and num

--- 01_basics/06_Numbers_math.js
+++ 01_basics/06_Numbers_math.js
@@ -18,3 +18,19 @@ console.log(hundreds.toLocaleString('en-IN'))
 /************************** Maths ************************/
 
 console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.sqrt(26).toFixed(4));
+console.log(Math.min(1, 4, 5, -1));
+
+// gives eveytime a random number between 0 to 1
+console.log(Math.random());
+console.log((Math.random()*10) + 1); // +1 to ignore the zero case
+console.log(Math.floor(Math.random()*10) + 1); // 1-9
+
+const min = 10;
+const max = 20;
+
+// for numbers between mit and max
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
