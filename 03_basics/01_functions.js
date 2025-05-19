fe99e32c8a56f9afe4ab6423c2ed86610a0b26fd
Aalok Kumar Rajwade <aalokkr.rajwade.cer21@itbhu.ac.in>
2025-05-19 18:44:25 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,5 +1,7 @@
 // functions: a package
 
+const { use } = require("react");
+
 // function add(num1, num2) {
 //     console.log(num1 + num2);
 // }
@@ -10,8 +12,8 @@ function add(num1, num2) {
 }
 console.log(add(1, 5));
 
-function loginUserMessage(username){
-    if(username == undefined){
+function loginUserMessage(username) {
+    if (username == undefined) {
         console.log("Please enter a username");
         return
     }
@@ -19,3 +21,40 @@ function loginUserMessage(username){
 }
 console.log(loginUserMessage());
 console.log(loginUserMessage("Happy"));
+
+// here ... is known as rest
+function calculateCartPrice(...num) {
+    return num;
+}
+console.log(calculateCartPrice(200, 100, 500));
+
+function calculateCartPrice2(val1, val2, ...num1) {
+    return num1;
+}
+console.log(calculateCartPrice2(200, 400, 500, 2000))
+
+// passing object in the function:
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user);
+
+handleObject({
+    username: "sam",
+    price: 200
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
