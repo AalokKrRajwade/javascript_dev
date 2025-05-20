0e0e5cc618927bf01a899fba6fb3fdb5efad165e
Aalok Kumar Rajwade <aalokkr.rajwade.cer21@itbhu.ac.in>
2025-05-20 20:12:05 +0000

    reduce intro

---
+++ 05_iterations/06_reduce.js
@@ -0,0 +1,22 @@
+// Array.prototype.reduce()
+// The reduce() method of Array instances executes a user-supplied "reducer" callback function on each 
+// element of the array, in order, passing in the return value from the calculation on the preceding 
+// element. The final result of running the reducer across all elements of the array is a single value.
+
+const myNums = [1, 2, 3, 4]
+
+// 0 + 1 + 2 + 3 + 4
+const myTotal = myNums.reduce(function (acc, currval) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    return acc + currval
+}, 0)
+
+// const myTotal = myNums.reduce((acc, curr) => acc + curr, 0)
+
+console.log(myTotal);
+// Expected output: 10
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
